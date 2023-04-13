09215638f2aaa639373078d3d684bae5f56fa4ed
Owen Palmer <speedcubegeek@gmail.com>
2023-04-12 22:25:16 -0700

    Basic start of tutorial

--- sections/section_1.js
+++ sections/section_1.js
@@ -1,18 +1,33 @@
 section_1 = `
+use ambient_api::{
+    components::core::{
+        app::main_scene,
+        camera::aspect_ratio_from_window,
+        primitives::quad,
+        transform::{lookat_center, translation},
+    },
+    concepts::{make_perspective_infinite_reverse_camera, make_transformable},
+    prelude::*,
+};
+
+#[main]
 pub async fn main() -> ResultEmpty {
     Entity::new()
-        .with_merge(make_transformable())
-        .with_default(sun())
+        .with_merge(make_perspective_infinite_reverse_camera())
+        .with(aspect_ratio_from_window(), EntityId::resources())
+        .with_default(main_scene())
+        .with(translation(), Vec3::ONE * 5.)
+        .with(lookat_center(), vec3(0., 0., 0.))
         .spawn();
 
-    spawn_query((player(), user_id())).bind(move |players| {
-        for (id, (_, user)) in players {
-            let camera = Entity::new()
-                .with_merge(make_perspective_infinite_reverse_camera())
-                .with(lookat_center(), vec3(0., 0., 0.))
-                .spawn();
+    Entity::new()
+        .with_merge(make_transformable())
+        .with_default(quad())
+        .spawn();
 
-        }
-    });
+    println!("Hello, Ambient!");
+
+    OkEmpty
 }
+
 `.trim();
--- sections/section_2.js
+++ sections/section_2.js
@@ -1,6 +1,9 @@
 section_2 = `
+use ambient_api::prelude::*;
+
+#[main]
 pub async fn main() -> ResultEmpty {
-        }
-    });
+    OkEmpty
 }
+
 `.trim();
